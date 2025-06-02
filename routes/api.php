38f3d93e15aa5bcab34fd69af51033ebfc099fa7
AhmedElHawary <ahmed.ashraf1823@gmail.com>
2025-06-02 16:54:26 +0300

    fix : APIs routes

--- routes/api.php
+++ routes/api.php
@@ -23,11 +23,11 @@ Route::group(['middleware' => 'api', 'prefix' =>'auth'],function($router){
     Route::post('/login',[AuthController::class,'login']);
     Route::get('/user-data',[AuthController::class,'userData'])->middleware('auth.guard');
     Route::post('/logout',[AuthController::class,'logout'])->middleware('auth.guard');
-    Route::post('/info1',[UserUpdateController::class,'aboutYourself'])->middleware('auth.guard');
-    Route::post('/info2',[UserUpdateController::class,'diabetesType'])->middleware('auth.guard');
-    Route::post('/profile-update',[UserUpdateController::class,'profileUpdate'])->middleware('auth.guard');
+    Route::put('/info1',[UserUpdateController::class,'aboutYourself'])->middleware('auth.guard');
+    Route::put('/info2',[UserUpdateController::class,'diabetesType'])->middleware('auth.guard');
+    Route::put('/profile-update',[UserUpdateController::class,'profileUpdate'])->middleware('auth.guard');
     Route::post('/add-companion',[CompanionLinkingController::class,'addCompanion'])->middleware('auth.guard');
-    Route::post('/delete-companion',[CompanionLinkingController::class,'deleteCompanion'])->middleware('auth.guard');
+    Route::delete('/delete-companion',[CompanionLinkingController::class,'deleteCompanion'])->middleware('auth.guard');
     Route::get('/get-companions',[CompanionLinkingController::class,'getCompanions'])->middleware('auth.guard');
     Route::get('/get-all-meals',[DietController::class,'getAllMeals'])->middleware('auth.guard');
     Route::post('/get-searched-meals',[DietController::class,'getSearchedMeals'])->middleware('auth.guard');
